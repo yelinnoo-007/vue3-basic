4881065e1d4f9690119a9973b2766ecd9724785e
dev <yelinnoo222000@gmail.com>
2024-07-29 21:20:33 +0630

    v-for and v-bind directive

--- app.js
+++ app.js
@@ -2,15 +2,43 @@ const app = Vue.createApp({
   // template: '<h2>Hello vue3</h2>',
   data() {
     return {
-      book: "Think like zuk",
-      author: "zucarburg",
-      age:25,
-    }
+      books: [
+        {
+          title: "Think like zuk",
+          author: "zucarburg",
+          age: 25,
+        },
+        {
+          title: "Think like zuk",
+          author: "zucarburg",
+          age: 25,
+        },
+        {
+          title: "Think like zuk",
+          author: "zucarburg",
+          age: 25,
+        },
+      ],
+      link:"http://www.google.com",
+      bookStatus: true,
+    };
   },
   methods: {
     increaseAge() {
       this.age++;
-    }
-  }
+    },
+    toogle() {
+      this.bookStatus = false;
+    },
+    mouseoverHandler(event, message) {
+      alert(event.type + message);
+    },
+    mouseleaveHandler(event) {
+      alert("mouse leave" + event.type);
+    },
+    doubleclickHandler(event) {
+      alert("double click" + event.type);
+    },
+  },
 });
-app.mount('#app');
+app.mount("#app");
